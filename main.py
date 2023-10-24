f701a87979c020bbb81640ca90988a194b424928
Anugrah <anugrahgupta.52@gmail.com>
2023-10-24 17:22:00 +0530

    day 35

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import html
+from dotenv import load_dotenv
+import os
+
+# from ui import QuizInterface
+import requests
+import pandas as pd
+from datetime import datetime, time
+from message import send_message
+
+rain_alert_icon=['10', '11', '12', '13', '14', '15', '20', '21', '22', '23', '24', '25']
+load_dotenv()
+api_key = os.environ['api_key']
 
 
-from question_model import Question
-from data import get_new_question
-from quiz_brain import QuizBrain
-from ui import QuizInterface
+def get_hourly_forecast(city_name):
 
+    parameters={'place_id': f'{city_name}',
+               'sections':'hourly',
+               'language': 'en',
+               'units': 'metric',
+               'key': api_key
+               
+               }
+    response= requests.get(url='https://www.meteosource.com/api/v1/free/point?', params=parameters)
+    response.raise_for_status()
+    return response.json()['hourly']['data']
 
+def rain_alert(city_name):
+    
+    # Get today's date
+    today_date = datetime.today().date()
 
-question_bank = []
-for question in get_new_question():
-    question_text = html.unescape(question["question"])
-    question_answer = html.unescape(question["correct_answer"])
-    incorrect_answers = [html.unescape(x) for x in question["incorrect_answers"]]
+    # Create a datetime object for 8 AM
+    going_off_time = datetime.combine(today_date, time(8, 0))
+    get_back_time = datetime.combine(today_date, time(19, 0))
 
-    new_question = Question(question_text, question_answer, incorrect_answers)
-    question_bank.append(new_question)
+    data=get_hourly_forecast(city_name)
+    df= pd.DataFrame(data)
+    df['date']=pd.to_datetime(df['date'])
+    df = df[(df['date'] >= going_off_time) & (df['date'] <= get_back_time)]
+    result = df['icon'].isin(rain_alert_icon).any()
+    if result:
+        send_message(mssg_body='Helllo! I thing today it will rain, so get your umberalla')
 
-
-quiz = QuizBrain(question_bank)
-
-# while quiz.still_has_questions():
-#     quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
-
-QuizInterface(quiz)
+# print(get_current_weather_info('indore', 'IN'))
+print(rain_alert('delhi'))
--- question_model.py
+++
@@ -1,8 +0,0 @@
-import random as rd
-class Question:
-
-    def __init__(self, q_text, q_answer, incorrect_answer):
-        self.text = q_text
-        self.answer = q_answer
-        self.options=incorrect_answer + [q_answer]
-        rd.shuffle(self.options)
